942e50647f97db0ab0e5116a8f8989c7607ecb46
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-20 08:52:24 +0200

    Re-add legacy code
    update-quality-legacy was used in tests, so it needs to be in this namespace

--- gilded-rose/src/gilded_rose/core.clj
+++ gilded-rose/src/gilded_rose/core.clj
@@ -69,3 +69,36 @@
     (update-quality inventory)))
 
 
+
+;; -----------------------------------------------------------------------------
+;; Legacy code, for use as an oracle
+
+(defn update-quality-legacy [items]
+  (map
+    (fn[item] (cond
+      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
+        (merge item {:quality 0})
+      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
+        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
+          (merge item {:quality (inc (inc (:quality item)))})
+          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
+            (merge item {:quality (inc (inc (inc (:quality item))))})
+            (if (< (:quality item) 50)
+              (merge item {:quality (inc (:quality item))})
+              item)))
+      (< (:sell-in item) 0)
+        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
+          (merge item {:quality 0})
+          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+            (merge item {:quality (- (:quality item) 2)})
+            item))
+      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
+        (merge item {:quality (dec (:quality item))})
+      :else item))
+  (map (fn [item]
+      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
+        (merge item {:sell-in (dec (:sell-in item))})
+        item))
+  items)))
+
+
--- gilded-rose/test/gilded_rose/core_test.clj
+++ gilded-rose/test/gilded_rose/core_test.clj
@@ -5,30 +5,29 @@
 
 (rose/update-current-inventory)
 
-(def test-output '({:name "+5 Dexterity Vest", :sell-in 9, :quality 19}
+(def test-output [{:name "+5 Dexterity Vest", :sell-in 9, :quality 19}
                   {:name "Aged Brie", :sell-in 1, :quality 1}
                   {:name "Elixir of the Mongoose", :sell-in 4, :quality 6}
                   {:name "Sulfuras, Hand Of Ragnaros", :sell-in -1, :quality 80}
-                  {:name "Backstage passes to a TAFKAL80ETC concert", :sell-in 14, :quality 21}))
+                  {:name "Backstage passes to a TAFKAL80ETC concert", :sell-in 14, :quality 21}])
 
 (deftest update-quality
   (testing "Testing update-quality"
-    (is (= test-output (rose/update-current-inventory))))
-)
+    (is (= test-output (rose/update-current-inventory)))))
 
 (deftest update-quality2
-  (testing "something usefull"
+  (testing "something useful"
     (let [test-input [{:name "Backstage passes to a TAFKAL80ETC concert"
                        :sell-in 0
                        :quality 10}]]
-      (is (= (rose/update-quality2 test-input)
+      (is (= (rose/update-quality-legacy test-input)
              (rose/update-quality test-input))))))
 
 (deftest testing-change-quality
   (testing "test quality"
     (let [test-input {:name "Backstage passes to a TAFKAL80ETC concert"
-                        :sell-in 1
-                        :quality 10}]
+                      :sell-in 1
+                      :quality 10}]
       (is (= (:quality (rose/change-quality test-input))
              13)))))
 
