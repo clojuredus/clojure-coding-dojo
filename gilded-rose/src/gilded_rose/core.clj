c4279b6d00ca333d13d4496049e13231497db0a6
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-20 08:01:26 +0200

    Gilded-Rose results of Clojure Meetup Düsseldorf

--- gilded-rose/src/gilded_rose/core.clj
+++ gilded-rose/src/gilded_rose/core.clj
@@ -1,44 +1,71 @@
-(ns gilded-rose.core)
+(ns gilded-rose.core
+  "Results of the Coding Dojo from the Clojure Meetup Düsseldorf.
+   Mode: Randori
+   Time: ~2 hours")
+
+(defn expired? [item]
+  (neg? (:sell-in item)))
+
+(defn quality-increase [item amount]
+  (update item :quality + amount))
+
+(defn quality-decrease [item amount]
+  (update item :quality - amount))
+
+(defn quality-reset [item]
+  (assoc item :quality 0))
+
+(defn age-item [item]
+  (if (= "Sulfuras, Hand of Ragnaros" (:name item))
+    item
+    (update item :sell-in dec)))
+
+;; -----------------------------------------------------------------------------
+;; Items
+
+(defmulti update-item :name)
+
+(defmethod update-item "Backstage passes to a TAFKAL80ETC concert" [item]
+  (cond
+    (< 4 (:sell-in item) 10) (quality-increase item 2)
+    (< -1 (:sell-in item) 5) (quality-increase item 3)
+    (expired? item) (quality-reset item)
+    :else (quality-increase item 1)))
+
+(defmethod update-item "Aged Brie" [item]
+  (if (< (:quality item) 50)
+    (quality-increase item 1)
+    item))
+
+(defmethod update-item "+5 Dexterity Vest" [item]
+  (if (expired? item)
+    (quality-decrease item 2)
+    (quality-decrease item 1)))
+
+(defmethod update-item "Elixir of the Mongoose" [item]
+  (if (expired? item)
+    (quality-decrease item 2)
+    (quality-decrease item 1)))
+
+(defmethod update-item :default [item] item)
+
+;; -----------------------------------------------------------------------------
+
+(def change-quality update-item)
 
 (defn update-quality [items]
-  (map
-    (fn[item] (cond
-      (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
-        (merge item {:quality 0})
-      (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
-        (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
-          (merge item {:quality (inc (inc (:quality item)))})
-          (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 0) (< (:sell-in item) 5))
-            (merge item {:quality (inc (inc (inc (:quality item))))})
-            (if (< (:quality item) 50)
-              (merge item {:quality (inc (:quality item))})
-              item)))
-      (< (:sell-in item) 0)
-        (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
-          (merge item {:quality 0})
-          (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-            (merge item {:quality (- (:quality item) 2)})
-            item))
-      (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
-        (merge item {:quality (dec (:quality item))})
-      :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+  (map (comp change-quality age-item) items))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
 
 (defn update-current-inventory[]
   (let [inventory
-    [
-      (item "+5 Dexterity Vest" 10 20)
-      (item "Aged Brie" 2 0)
-      (item "Elixir of the Mongoose" 5 7)
-      (item "Sulfuras, Hand Of Ragnaros" 0 80)
-      (item "Backstage passes to a TAFKAL80ETC concert" 15 20)
-    ]]
-    (update-quality inventory)
-    ))
+        [(item "+5 Dexterity Vest" 10 20)
+         (item "Aged Brie" 2 0)
+         (item "Elixir of the Mongoose" 5 7)
+         (item "Sulfuras, Hand Of Ragnaros" 0 80)
+         (item "Backstage passes to a TAFKAL80ETC concert" 15 20)]]
+    (update-quality inventory)))
+
+
--- gilded-rose/test/gilded_rose/core_test.clj
+++ gilded-rose/test/gilded_rose/core_test.clj
@@ -3,6 +3,32 @@
             [clojure.spec.test :as stest]
             [gilded-rose.core :as rose]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(rose/update-current-inventory)
+
+(def test-output '({:name "+5 Dexterity Vest", :sell-in 9, :quality 19}
+                  {:name "Aged Brie", :sell-in 1, :quality 1}
+                  {:name "Elixir of the Mongoose", :sell-in 4, :quality 6}
+                  {:name "Sulfuras, Hand Of Ragnaros", :sell-in -1, :quality 80}
+                  {:name "Backstage passes to a TAFKAL80ETC concert", :sell-in 14, :quality 21}))
+
+(deftest update-quality
+  (testing "Testing update-quality"
+    (is (= test-output (rose/update-current-inventory))))
+)
+
+(deftest update-quality2
+  (testing "something usefull"
+    (let [test-input [{:name "Backstage passes to a TAFKAL80ETC concert"
+                       :sell-in 0
+                       :quality 10}]]
+      (is (= (rose/update-quality2 test-input)
+             (rose/update-quality test-input))))))
+
+(deftest testing-change-quality
+  (testing "test quality"
+    (let [test-input {:name "Backstage passes to a TAFKAL80ETC concert"
+                        :sell-in 1
+                        :quality 10}]
+      (is (= (:quality (rose/change-quality test-input))
+             13)))))
+
