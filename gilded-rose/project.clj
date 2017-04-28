b860c2cba61c0dfeb25bc9cf99f999cbc963bd91
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-04-28 15:35:49 +0200

    Update Clojure, use new spec namespaces

--- gilded-rose/project.clj
+++ gilded-rose/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
                  [org.clojure/test.check "0.9.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]
   :main ^:skip-aot gilded-rose.core
