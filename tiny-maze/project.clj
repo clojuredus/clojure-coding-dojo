1e074d0a67320327d0abda31fe9508171821e27d
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-05-23 17:25:20 +0200

    Add tiny-maze kata

---
+++ tiny-maze/project.clj
@@ -0,0 +1,8 @@
+(defproject tiny-maze "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]
+  :main tiny-maze.solver)
---
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -0,0 +1,7 @@
+(ns tiny-maze.solver
+  "- _S_ : start of the maze
+  - _E_ : end of the maze
+  - _1_ : This is a wall that you cannot pass through
+  - _0_ : A free space that you can move through.")
+
+(defn solve-maze [maze])
