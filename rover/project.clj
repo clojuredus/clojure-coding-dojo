d2379278ef48b0c811f981dfb5df57d3b65129f4
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-05-24 13:05:30 +0200

    Add rover kata

---
+++ rover/project.clj
@@ -0,0 +1,9 @@
+(defproject rover "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [myguidingstar/clansi "1.3.0"]]
+  :main rover.console
+  :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]])
