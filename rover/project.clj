672f17750a4d807c902f538e8a8ee0b74f8feeee
Christian Meter <christian.meter@uni-duesseldorf.de>
2018-11-24 10:37:54 +0100

    Update clojure version

--- rover/project.clj
+++ rover/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/test.check "0.9.0"]
                  [myguidingstar/clansi "1.3.0"]]
   :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]
