3b56e7e5df1cbc8ee0d0befcb5f03fec02d8c81e
Christian Meter <cmeter@googlemail.com>
2019-09-12 16:40:05 +0200

    Update dependencies

--- gilded-rose/project.clj
+++ gilded-rose/project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [org.clojure/test.check "0.9.0"]]
-  :plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]
-            [nightlight/lein-nightlight "1.9.3"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/test.check "0.10.0"]]
+  :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
+            [nightlight/lein-nightlight "2.4.2"]]
   :main ^:skip-aot gilded-rose.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
